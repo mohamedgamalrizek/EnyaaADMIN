03ec4c5023d6347ba2013ce574309ddc84f3b1f0
Mohamed Gamal <info@mohamed-gamal.com>
2026-08-04 22:56:48 +0300

    Public URL

--- src/Pages/Settings/Configs/SettingConfigurations.jsx
+++ src/Pages/Settings/Configs/SettingConfigurations.jsx
@@ -42,8 +42,8 @@ function SettingConfigurations({ groupName }) {
   const getData = async () => {
     await r();
     const res = await GET(
-      admin.token,
-      `get_configurations/group_name/${groupName}`
+        admin.token,
+        `get_configurations/group_name/${groupName}`
     );
 
     if (res.response === 200 && Array.isArray(res.data)) {
@@ -72,121 +72,121 @@ function SettingConfigurations({ groupName }) {
     }
   }
   return (
-    <Box>
-      {isLoading || !data ? (
-        <Box mt={10}>
-          <Skeleton h={300} w={"100%"} />
-        </Box>
-      ) : (
-        <Box maxWidth="100%" overflowX="auto" mt={10}>
-          <TableContainer
-            border={"1px solid"}
-            borderColor={useColorModeValue("gray.100", "gray.600")}
-            borderRadius={"lg"}
-            padding={3}
-          >
-            <Table
-              variant="simple"
-              colorScheme="gray"
-              fontSize={12}
-              size={"sm"}
-              fontWeight={500}
-            >
-              <Thead background={useColorModeValue("blue.50", "blue.700")}>
-                <Tr color={"#000"}>
-                  <Th
-                    color={useColorModeValue("#000", "#fff")}
-                    py={3}
-                    padding={"8px 8px"}
-                    textAlign={"center"}
-                  >
-                    ACTION
-                  </Th>
-                  {["ID", "Titile", "Value", "Last Update"].map((item) => (
-                    <Th
-                      key={item}
-                      color={useColorModeValue("#000", "#fff")}
-                      py={3}
-                      padding={"8px 8px"}
-                    >
-                      {item}
-                    </Th>
-                  ))}
-                </Tr>
-              </Thead>
-              <Tbody>
-                {data.map((item) => (
-                  <Tr key={item.id}>
-                    <Td padding={"12px"}>
-                      {" "}
-                      <Flex justify={"center"}>
-                        <IconButton
-                          isDisabled={
-                            item.value === "true" ||
+      <Box>
+        {isLoading || !data ? (
+            <Box mt={10}>
+              <Skeleton h={300} w={"100%"} />
+            </Box>
+        ) : (
+            <Box maxWidth="100%" overflowX="auto" mt={10}>
+              <TableContainer
+                  border={"1px solid"}
+                  borderColor={useColorModeValue("gray.100", "gray.600")}
+                  borderRadius={"lg"}
+                  padding={3}
+              >
+                <Table
+                    variant="simple"
+                    colorScheme="gray"
+                    fontSize={12}
+                    size={"sm"}
+                    fontWeight={500}
+                >
+                  <Thead background={useColorModeValue("blue.50", "blue.700")}>
+                    <Tr color={"#000"}>
+                      <Th
+                          color={useColorModeValue("#000", "#fff")}
+                          py={3}
+                          padding={"8px 8px"}
+                          textAlign={"center"}
+                      >
+                        ACTION
+                      </Th>
+                      {["ID", "Titile", "Value", "Last Update"].map((item) => (
+                          <Th
+                              key={item}
+                              color={useColorModeValue("#000", "#fff")}
+                              py={3}
+                              padding={"8px 8px"}
+                          >
+                            {item}
+                          </Th>
+                      ))}
+                    </Tr>
+                  </Thead>
+                  <Tbody>
+                    {data.map((item) => (
+                        <Tr key={item.id}>
+                          <Td padding={"12px"}>
+                            {" "}
+                            <Flex justify={"center"}>
+                              <IconButton
+                                  isDisabled={
+                                      item.value === "true" ||
+                                      item.value === "false" ||
+                                      item.value === true ||
+                                      item.value === false
+                                  }
+                                  size={"sm"}
+                                  variant={"ghost"}
+                                  _hover={{
+                                    background: "none",
+                                  }}
+                                  onClick={() => {
+                                    setSelectedData(item);
+                                    EditonOpen();
+                                  }}
+                                  icon={
+                                    <FiEdit
+                                        fontSize={18}
+                                        color={theme.colors.blue[500]}
+                                    />
+                                  }
+                              />
+                            </Flex>
+                          </Td>
+                          <Td padding={"12px"}>{item.id}</Td>
+                          <Td padding={"12px"}>{item.title}</Td>
+                          <Td padding={"12px"} maxW={500} overflow={"hidden"}>
+                            {item.id_name === "fav_icon" ||
+                            item.id_name === "logo" ||
+                            item?.id_name === "ma_doctor_image" ||
+                            item?.id_name === "web_doctor_image" ? (
+                                <Image
+                                    src={`${imageBaseURL}/${item.value}`}
+                                    w={12}
+                                    fallbackSrc="imagePlaceholder.png"
+                                />
+                            ) : item.value === "true" ||
                             item.value === "false" ||
                             item.value === true ||
-                            item.value === false
-                          }
-                          size={"sm"}
-                          variant={"ghost"}
-                          _hover={{
-                            background: "none",
-                          }}
-                          onClick={() => {
-                            setSelectedData(item);
-                            EditonOpen();
-                          }}
-                          icon={
-                            <FiEdit
-                              fontSize={18}
-                              color={theme.colors.blue[500]}
-                            />
-                          }
-                        />
-                      </Flex>
-                    </Td>
-                    <Td padding={"12px"}>{item.id}</Td>
-                    <Td padding={"12px"}>{item.title}</Td>
-                    <Td padding={"12px"} maxW={500} overflow={"hidden"}>
-                      {item.id_name === "fav_icon" ||
-                      item.id_name === "logo" ||
-                      item?.id_name === "ma_doctor_image" ||
-                      item?.id_name === "web_doctor_image" ? (
-                        <Image
-                          src={`${imageBaseURL}/${item.value}`}
-                          w={12}
-                          fallbackSrc="imagePlaceholder.png"
-                        />
-                      ) : item.value === "true" ||
-                        item.value === "false" ||
-                        item.value === true ||
-                        item.value === false ? (
-                        <IsActive
-                          id={item.id}
-                          isActive={item.value}
-                          groupName={item.group_name}
-                        />
-                      ) : (
-                        item.value
-                      )}
-                    </Td>
-                    <Td padding={"12px"}>{item.updated_at}</Td>
-                  </Tr>
-                ))}
-              </Tbody>
-            </Table>
-          </TableContainer>
-        </Box>
-      )}
+                            item.value === false ? (
+                                <IsActive
+                                    id={item.id}
+                                    isActive={item.value}
+                                    groupName={item.group_name}
+                                />
+                            ) : (
+                                item.value
+                            )}
+                          </Td>
+                          <Td padding={"12px"}>{item.updated_at}</Td>
+                        </Tr>
+                    ))}
+                  </Tbody>
+                </Table>
+              </TableContainer>
+            </Box>
+        )}
 
-      {EditisOpen && (
-        <UpdateConfigs
-          data={SelectedData}
-          isOpen={EditisOpen}
-          onClose={EditonClose}
-        />
-      )}
-    </Box>
+        {EditisOpen && (
+            <UpdateConfigs
+                data={SelectedData}
+                isOpen={EditisOpen}
+                onClose={EditonClose}
+            />
+        )}
+      </Box>
   );
 }
 
@@ -221,16 +221,16 @@ const IsActive = ({ id, isActive, groupName }) => {
   });
 
   return (
-    <FormControl display="flex" alignItems="center">
-      <Switch
-        defaultChecked={isActive === "true"}
-        size={"md"}
-        onChange={(e) => {
-          let value = e.target.checked ? "true" : "false";
+      <FormControl display="flex" alignItems="center">
+        <Switch
+            defaultChecked={isActive === "true"}
+            size={"md"}
+            onChange={(e) => {
+              let value = e.target.checked ? "true" : "false";
 
-          mutation.mutate({ id, value });
-        }}
-      />
-    </FormControl>
+              mutation.mutate({ id, value });
+            }}
+        />
+      </FormControl>
   );
 };
--- src/Pages/Settings/Configs/UpdateConfigs.jsx
+++ src/Pages/Settings/Configs/UpdateConfigs.jsx
@@ -59,11 +59,11 @@ export default function UpdateConfigs({ isOpen, onClose, data }) {
 
   const handleUpdate = async (Inputdata) => {
     let formData = selectedFile
-      ? {
+        ? {
           value: selectedFile,
           id: data.id,
         }
-      : {
+        : {
           ...Inputdata,
           id: data.id,
         };
@@ -96,9 +96,9 @@ export default function UpdateConfigs({ isOpen, onClose, data }) {
     try {
       setisDeleteLoading(true);
       const res = await DELETE(
-        admin.token,
-        "remove_configurations_image",
-        formData
+          admin.token,
+          "remove_configurations_image",
+          formData
       );
       setisDeleteLoading(false);
       if (res.response === 200) {
@@ -117,134 +117,134 @@ export default function UpdateConfigs({ isOpen, onClose, data }) {
   };
 
   return (
-    <Modal
-      isOpen={isOpen}
-      onClose={onClose}
-      isCentered
-      size={"lg"}
-      scrollBehavior="inside"
-    >
-      <ModalOverlay />
-      <ModalContent as={"form"} onSubmit={handleSubmit(handleUpdate)}>
-        <ModalHeader fontSize={18} py={2}>
-          Update {data.group_name}
-        </ModalHeader>
-        <ModalCloseButton />
-        <Divider />
-        <ModalBody>
-          <Box pb={3}>
-            <FormControl>
-              <FormLabel>Title</FormLabel>
-              <Input
-                defaultValue={data?.title}
-                placeholder="Title"
-                {...register("title")}
-                isReadOnly
-              />
-            </FormControl>
-
-            {data?.id_name === "fav_icon" ||
-            data?.id_name === "logo" ||
-            data?.id_name === "ma_doctor_image" ||
-            data?.id_name === "web_doctor_image" ? (
-              data.value !== null ? (
-                <Flex mt={5} align={"center"} gap={5}>
-                  <Image
-                    src={`${imageBaseURL}/${data?.value}`}
-                    width={100}
-                    borderRadius={5}
-                  />
-                  <Button
-                    size={"sm"}
-                    colorScheme="red"
-                    fontSize={12}
-                    isLoading={isDeleteLoading}
-                    onClick={DeleteImage}
-                  >
-                    Remove Image
-                  </Button>
-                </Flex>
-              ) : (
-                <Box
-                  mt={5}
-                  p={4}
-                  border="2px dashed"
-                  borderColor="gray.300"
-                  borderRadius="md"
-                  onDrop={handleDrop}
-                  onDragOver={handleDragOver}
-                  onClick={() => {
-                    if (fileInputRef.current) {
-                      fileInputRef.current.click();
-                    }
-                  }}
-                  cursor={"pointer"}
-                >
-                  {selectedFile ? (
-                    <Box position={"relative"}>
-                      <Text>Selected File: {selectedFile.name}</Text>
-                      <CloseButton
-                        position={"absolute"}
-                        right={-2}
-                        top={-2}
-                        size={"sm"}
-                        onClick={() => {
-                          setSelectedFile(null);
-                        }}
-                      />
-                    </Box>
-                  ) : (
-                    <Box>
-                      <VisuallyHidden>
-                        {" "}
-                        <Input
-                          ref={fileInputRef}
-                          type="file"
-                          onChange={handleFileChange}
-                          accept=".jpeg, .svg, .png , .jpg"
-                          mb={4}
-                        />
-                      </VisuallyHidden>
-
-                      <Center>
-                        {" "}
-                        <AiOutlineUpload fontSize={32} />
-                      </Center>
-                      <Text textAlign={"center"} mt={3}>
-                        <b>Choose a file</b> or Drag it here.
-                      </Text>
-                    </Box>
-                  )}
-                </Box>
-              )
-            ) : (
-              <FormControl isRequired mt={5}>
-                <FormLabel>Value</FormLabel>
-                <Textarea
-                  defaultValue={data?.value}
-                  placeholder="URL"
-                  {...register("value", { required: true })}
+      <Modal
+          isOpen={isOpen}
+          onClose={onClose}
+          isCentered
+          size={"lg"}
+          scrollBehavior="inside"
+      >
+        <ModalOverlay />
+        <ModalContent as={"form"} onSubmit={handleSubmit(handleUpdate)}>
+          <ModalHeader fontSize={18} py={2}>
+            Update {data.group_name}
+          </ModalHeader>
+          <ModalCloseButton />
+          <Divider />
+          <ModalBody>
+            <Box pb={3}>
+              <FormControl>
+                <FormLabel>Title</FormLabel>
+                <Input
+                    defaultValue={data?.title}
+                    placeholder="Title"
+                    {...register("title")}
+                    isReadOnly
                 />
               </FormControl>
-            )}
-          </Box>
-        </ModalBody>
-        <Divider />
-        <ModalFooter py={3}>
-          <Button colorScheme="gray" mr={3} onClick={onClose} size={"sm"}>
-            Close
-          </Button>
-          <Button
-            variant="solid"
-            size={"sm"}
-            colorScheme="blue"
-            type="submit"
-            isLoading={isLoading}
-          >
-            Update
-          </Button>
-        </ModalFooter>
-      </ModalContent>
-    </Modal>
+
+              {data?.id_name === "fav_icon" ||
+              data?.id_name === "logo" ||
+              data?.id_name === "ma_doctor_image" ||
+              data?.id_name === "web_doctor_image" ? (
+                  data.value !== null ? (
+                      <Flex mt={5} align={"center"} gap={5}>
+                        <Image
+                            src={`${imageBaseURL}/${data?.value}`}
+                            width={100}
+                            borderRadius={5}
+                        />
+                        <Button
+                            size={"sm"}
+                            colorScheme="red"
+                            fontSize={12}
+                            isLoading={isDeleteLoading}
+                            onClick={DeleteImage}
+                        >
+                          Remove Image
+                        </Button>
+                      </Flex>
+                  ) : (
+                      <Box
+                          mt={5}
+                          p={4}
+                          border="2px dashed"
+                          borderColor="gray.300"
+                          borderRadius="md"
+                          onDrop={handleDrop}
+                          onDragOver={handleDragOver}
+                          onClick={() => {
+                            if (fileInputRef.current) {
+                              fileInputRef.current.click();
+                            }
+                          }}
+                          cursor={"pointer"}
+                      >
+                        {selectedFile ? (
+                            <Box position={"relative"}>
+                              <Text>Selected File: {selectedFile.name}</Text>
+                              <CloseButton
+                                  position={"absolute"}
+                                  right={-2}
+                                  top={-2}
+                                  size={"sm"}
+                                  onClick={() => {
+                                    setSelectedFile(null);
+                                  }}
+                              />
+                            </Box>
+                        ) : (
+                            <Box>
+                              <VisuallyHidden>
+                                {" "}
+                                <Input
+                                    ref={fileInputRef}
+                                    type="file"
+                                    onChange={handleFileChange}
+                                    accept=".jpeg, .svg, .png , .jpg"
+                                    mb={4}
+                                />
+                              </VisuallyHidden>
+
+                              <Center>
+                                {" "}
+                                <AiOutlineUpload fontSize={32} />
+                              </Center>
+                              <Text textAlign={"center"} mt={3}>
+                                <b>Choose a file</b> or Drag it here.
+                              </Text>
+                            </Box>
+                        )}
+                      </Box>
+                  )
+              ) : (
+                  <FormControl isRequired mt={5}>
+                    <FormLabel>Value</FormLabel>
+                    <Textarea
+                        defaultValue={data?.value}
+                        placeholder="URL"
+                        {...register("value", { required: true })}
+                    />
+                  </FormControl>
+              )}
+            </Box>
+          </ModalBody>
+          <Divider />
+          <ModalFooter py={3}>
+            <Button colorScheme="gray" mr={3} onClick={onClose} size={"sm"}>
+              Close
+            </Button>
+            <Button
+                variant="solid"
+                size={"sm"}
+                colorScheme="blue"
+                type="submit"
+                isLoading={isLoading}
+            >
+              Update
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
   );
 }
